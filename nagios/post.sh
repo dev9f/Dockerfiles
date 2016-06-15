178ce2dc7519ca11e1b85e386d773637401c6dcc
root <root@docker.acs>
2016-06-15 14:42:32 +0900

    backup deleted & modify post.sh

--- nagios/post.sh
+++ nagios/post.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
 
 
-export APP_HOME="/app"
-export WORK="/work"
-export NAGIOS_HOME="/app/nagios"
-export GRAPHIOS_HOME="/app/nagios/graphios"
-export GRAPHIOS_LOGS="/app/nagios/graphios/logs"
-export GRAPHIOS_SPOOL="/var/spool/nagios/graphios"
-export INFLUXDB_SERVER="influxdb"
-export INFLUXDB_DB="stigma"
-export INFLUXDB_USER="stigma"
-export INFLUXDB_PASS="stigma"
+# Nagios & Graphios ENV Check
+set -m
+: "${APP_HOME:=/app}"
+: "${WORK:=/work}"
+: "${NAGIOS_HOME:=/app/nagios}"
+: "${GRAPHIOS_HOME:=/app/nagios/graphios}"
+: "${GRAPHIOS_LOGS:=/app/nagios/graphios/logs}"
+: "${GRAPHIOS_SPOOL:=/var/spool/nagios/graphios}"
+: "${GRAPHIOS_USED:=y}"
+: "${INFLUXDB_SERVER:=influxdb}"
+: "${INFLUXDB_DB:=stigma}"
+: "${INFLUXDB_USER:=stigma}"
+: "${INFLUXDB_PASS:=stigma}"
 
+# Nagios home directory check
 
 if [ -e ${NAGIOS_HOME} ]
 then
     echo "${NAGIOS_HOME} already exists."
 else
     echo "${NAGIOS_HOME} does not exists."
-    tar xvfz ${WORK}/nagios.tar.gz
+    tar xvfz ${WORK}/nagios.tar.gz -C /
 fi
 
 
+
  ## Graphios Start
-nohup  ${GRAPHIOS_HOME}/graphios.py -v --backend=influxdb --config_file=${GRAPHIOS_HOME}/graphios.cfg &
+if [ "${GRAPHIOS_USED}" = "y" ]
+then
+	nohup  ${GRAPHIOS_HOME}/graphios.py -v --backend=influxdb --config_file=${GRAPHIOS_HOME}/graphios.cfg 1> /dev/null 2>&1 & 
+fi
 
 # Nagios Start
 ${NAGIOS_HOME}/bin/nagios -d ${NAGIOS_HOME}/etc/nagios.cfg
 
 #httpd start
 apachectl -f /etc/httpd/conf/httpd.conf -DFOREGROUND
+
+
+
+
