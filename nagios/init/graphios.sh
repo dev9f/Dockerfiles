fae2ecff87beea27092f120b7cbb6cbc7bcb5f09
root <root@docker.acs>
2016-06-15 16:22:19 +0900

    2016-06-15

--- nagios/init/graphios.sh
+++ nagios/init/graphios.sh
@@ -12,38 +12,8 @@ git clone https://github.com/shawn-sterling/graphios.git
 cd ${WORK}/graphios
 
 mkdir -p ${GRAPHIOS_HOME}
-#mkdir -p ${GRAPHIOS_HOME}/logs
 
 cp graphios*.py ${GRAPHIOS_HOME}
 cp graphios.cfg ${GRAPHIOS_HOME}
-
-#mkdir -p ${GRAPHIOS_SPOOL}
-
-#chown -R nagios:nagcmd ${GRAPHIOS_SPOOL}
-#chmod 755 ${GRAPHIOS_SPOOL}
-
-#cp ${NAGIOS_HOME}/etc/nagios.cfg ${NAGIOS_HOME}/etc/nagios.cfg.org
-#echo "## Graphios Command" >> ${NAGIOS_HOME}/etc/nagios.cfg
-#echo "cfg_file=${NAGIOS_HOME}/etc/objects/graphios_commands.cfg" >> ${NAGIOS_HOME}/etc/nagios.cfg
-#sed -i 's/process_performance_data=0/process_performance_data=1/g' ${NAGIOS_HOME}/etc/nagios.cfg
-#cat ${NAGIOS_CONF}/graphios_commands.txt >> ${NAGIOS_HOME}/etc/nagios.cfg
-#sed -i "s|###GRAPHIOS_SPOOL###|${GRAPHIOS_SPOOL}|g" ${NAGIOS_HOME}/etc/nagios.cfg
-#cp ${NAGIOS_CONF}/graphios_commands.cfg ${NAGIOS_HOME}/etc/objects/
-#sed -i "s|###GRAPHIOS_SPOOL###|${GRAPHIOS_SPOOL}|g" ${NAGIOS_HOME}/etc/objects/graphios_commands.cfg
-
-
-#post config
-#cp ${GRAPHIOS_HOME}/graphios.cfg ${GRAPHIOS_HOME}/graphios.cfg.org
-#sed -i 's/enable_influxdb = False/enable_influxdb = True/g' ${GRAPHIOS_HOME}/graphios.cfg
-#echo "## InfluxDB Information of Nagios Status Data" >> ${GRAPHIOS_HOME}/graphios.cfg
-#echo "influxdb_servers = $INFLUXDB_SERVER:8086" >> ${GRAPHIOS_HOME}/graphios.cfg
-#echo "influxdb_db = $INFLUXDB_DB" >> ${GRAPHIOS_HOME}/graphios.cfg
-#echo "influxdb_user = $INFLUXDB_USER" >> ${GRAPHIOS_HOME}/graphios.cfg
-#echo "influxdb_password = $INFLUXDB_PASS" >> ${GRAPHIOS_HOME}/graphios.cfg
-#cat ${NAGIOS_CONF}/localhost.cfg > ${NAGIOS_HOME}/etc/objects/localhost.cfg
-#sed -i "s|###PREFIX_LOCALHOST###|${PREFIX_LOCALHOST}|g" ${NAGIOS_HOME}/etc/objects/localhost.cfg
-
-#Log location
-#sed -i "s|\/usr\/local\/nagios\/var\/graphios.log|${GRAPHIOS_LOGS}\/graphios.log|g" ${GRAPHIOS_HOME}/graphios.cfg
 
 
--- nagios/init/nagiosInstall.sh
+++ nagios/init/nagiosInstall.sh
@@ -6,9 +6,7 @@ echo "Nagios core Install"
 
 cd ${NAGIOS_HOME}
 wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.1.1.tar.gz
-wget http://www.nagios-plugins.org/download/nagios-plugins-2.1.1.tar.gz
 tar zxf nagios-4.1.1.tar.gz
-tar zxf nagios-plugins-2.1.1.tar.gz
 cd nagios-4.1.1
 ./configure --prefix=${NAGIOS_HOME} --with-command-group=nagcmd
 make all
