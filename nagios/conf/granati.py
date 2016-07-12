9f767e78ec85b2a93df0b0153e82088cc8dc8967
suhyun <sh@suhyuns-MacBook-Pro.local>
2016-07-12 17:59:58 +0900

    Executing modules as scripts

--- nagios/conf/granati.py
+++ nagios/conf/granati.py
@@ -2,4 +2,8 @@
 # vim: set ts=4 sts=4 sw=4 tw=79 et :
 # Author: Suhyun Lee <ilyf28@gmail.com> 
 
-print("Hello World")
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
