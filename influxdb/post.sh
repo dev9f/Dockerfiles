7db28ba72cf7245a89bbe3f9717d5f9dc70a7e5b
suhyun Lee <ilyf28@gmail.com>
2016-07-15 19:47:48 +0900

    modify request method

--- influxdb/post.sh
+++ influxdb/post.sh
@@ -19,16 +19,11 @@ function initialize_database() {
 
     start_bg_influxdb
 
-    #echo "+++++ Creating database users: ${IFDB_INIT_DB_USER_NM}"
-    #curl -G ${IFDB_API_URL}/query -u root:root --data-urlencode "q=CREATE USER ${IFDB_INIT_DB_USER_NM} WITH PASSWORD '${IFDB_INIT_DB_USER_PWD}' WITH ALL PRIVILEGES"
-
     echo "+++++ Creating database: ${IFDB_INIT_DB}"
-    curl -G ${IFDB_API_URL}/query --data-urlencode "q=CREATE DATABASE ${IFDB_INIT_DB}"
-    #curl -G ${IFDB_API_URL}/query -u ${IFDB_INIT_DB_USER_NM}:${IFDB_INIT_DB_USER_PWD} --data-urlencode "q=CREATE DATABASE ${IFDB_INIT_DB}"
+    curl -POST ${IFDB_API_URL}/query --data-urlencode "q=CREATE DATABASE ${IFDB_INIT_DB}"
 
     echo "+++++ Writing sample data"
     curl -i -XPOST ${IFDB_API_URL}/write?db=${IFDB_INIT_DB} --data-binary 'initialize_database value=0'
-    #curl -i -XPOST ${IFDB_API_URL}/write?db=${IFDB_INIT_DB} -u ${IFDB_INIT_DB_USER_NM}:${IFDB_INIT_DB_USER_PWD} --data-binary 'initialize_database value=0'
 
     stop_bg_influxdb
 }
