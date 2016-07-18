5eab39ea140a3a96b22fa16ce38adb4332d2f9b2
suhyun Lee <ilyf28@gmail.com>
2016-07-19 00:02:50 +0900

    modify port

--- nagios/api/app.js
+++ nagios/api/app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8888;
+var port = process.env.PORT || ###NAGIOS_API_PORT###;
 
 var routes = require('./routes.js')(app);
 
