f4dad86d161a7a8aa1626acc65ccb73a07dd17fe
suhyun <sh@suhyuns-MacBook-Pro.local>
2016-11-03 16:30:22 +0900

    modified runable on centos 7

--- mysql/post.sh
+++ mysql/post.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#APP_HOME="/app"
-#WORK="/work"
-
 set -m
 : "${MYSQL_HOME:=/app/mysql}"
 : "${MYSQL_LOGS:=/app/mysql/logs}"
@@ -13,14 +10,23 @@ set -m
 : "${MYSQL_DATABASE:=stigma}"
 
 function makedir() {
-       mkdir -p ${MYSQL_HOME}/logs
-       mkdir -p ${MYSQL_HOME}/conf
+       mkdir -p ${MYSQL_LOGS}
+       mkdir -p ${MYSQL_CONF}
 }
 
 function initialize_database() {
-       # Mysql_install_db & Start mysql_safe in the background
        mysql_install_db --defaults-file=${MYSQL_CONF}/my.cnf
        chown -R mysql:mysql ${MYSQL_DATADIR}
+}
+
+function set_root_password() {
+       mysqladmin -u root password ${MYSQL_ROOT_PASSWORD}
+}
+
+function create_database() {
+        mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create database ${MYSQL_DATABASE}"
+        mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'"
+        mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES"
 }
 
 function bg_stop() {
@@ -29,53 +35,36 @@ function bg_stop() {
        sleep 3
 }
 
-function check_root_password() {
-
-       # MYSQL_ROOT_PASSWORD
-#       if [ -z ${MYSQL_ROOT_PASSWORD} ]; then
-#               MYSQL_ROOT_PASSWORD=password
-#               echo 'root password is ${MYSQL_ROOT_PASSWORD}'
-#       fi
-       mysqladmin -u root password ${MYSQL_ROOT_PASSWORD}
-}
-
-function create_database() {
-
-	#Create database
-        mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create database ${MYSQL_DATABASE}"
-        mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'"
-        mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES"
-}
-
 if [ -e ${MYSQL_HOME} ]; then
         echo "${MYSQL_HOME} already exists ..."
 else
         echo "${MYSQL_HOME} does not exists ..."
         makedir
-        cp ${WORK}/my.cnf $MYSQL_CONF/my.cnf
+        cp ${WORK}/my.cnf ${MYSQL_CONF}/my.cnf
 fi
 
 if [ ! -d "${MYSQL_DATADIR}" ]; then
-	echo "Initializing Database..."
-   	initialize_database
+        echo "Initializing Database..."
+        initialize_database
 else
-   	echo "Do not initialize Database.Just start..."
+        echo "${MYSQL_DATADIR} already initialized. Just start..."
 fi
 
-# Start app in the background
-/usr/bin/mysqld_safe --defaults-file=${MYSQL_CONF}/my.cnf --datadir=${MYSQL_DATADIR} --basedir=${MYSQL_HOME} --language=${MYSQL_LANG}/english &
-sleep 3
-     
-check_root_password
-      
-if [ ! -z ${MYSQL_DATABASE} ]; then
-	create_database
+if [ ! -d ${MYSQL_DATADIR}/${MYSQL_DATABASE} ]; then
+        # Start app in the background
+        /usr/bin/mysqld_safe --defaults-file=${MYSQL_CONF}/my.cnf --datadir=${MYSQL_DATADIR} --basedir=${MYSQL_HOME} --language=${MYSQL_LANG}/english &
+        sleep 3
+
+        echo "Set root password ..."
+        set_root_password
+
+        echo "Create ${MYSQL_DATABASE} Database ..."
+        create_database
+
+        bg_stop
 else
-	echo "No create Database"
+        echo "${MYSQL_DATABASE} Database already exists ..."
 fi
-	
-bg_stop
-       
+
 # Start app in the foreground
 /usr/bin/mysqld_safe --defaults-file=${MYSQL_CONF}/my.cnf --datadir=${MYSQL_DATADIR} --basedir=${MYSQL_HOME} --language=${MYSQL_LANG}/english
-
