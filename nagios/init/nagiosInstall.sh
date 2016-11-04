30c5facf54e55874e7339b4e8a80cc968f6dc169
suhyun <sh@suhyuns-MacBook-Pro.local>
2016-11-04 12:43:20 +0900

    modified nagios version to 4.2.2

--- nagios/init/nagiosInstall.sh
+++ nagios/init/nagiosInstall.sh
@@ -5,13 +5,13 @@ export NAGIOS_HOME="/app/nagios"
 echo "Nagios core Install"
 
 cd ${NAGIOS_HOME}
-wget https://assets.nagios.com/downloads/nagioscore/releases/nagios-4.2.0.tar.gz
-tar xvf nagios-*.tar.gz
-cd nagios-*
-./configure --with-command-group=nagcmd --prefix=${NAGIOS_HOME}
+wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.2.2.tar.gz
+tar xzf nagioscore.tar.gz
+cd nagioscore-nagios-*
+./configure --prefix=${NAGIOS_HOME}
 make all
 make install
-make install-commandmode
 make install-init
+make install-commandmode
 make install-config
 make install-webconf
