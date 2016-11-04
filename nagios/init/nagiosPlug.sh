fc8f7744b13f48e88d081d71bd4265a57e3a15f5
suhyun <sh@suhyuns-MacBook-Pro.local>
2016-11-04 11:16:15 +0900

    modified nagios plugins version at Latest stable release

--- nagios/init/nagiosPlug.sh
+++ nagios/init/nagiosPlug.sh
@@ -5,7 +5,7 @@ export NAGIOS_HOME="/app/nagios"
 echo "Nagios Plugins Install"
 
 cd ${NAGIOS_HOME}
-wget http://nagios-plugins.org/download/nagios-plugins-2.1.3.tar.gz
+wget http://nagios-plugins.org/download/nagios-plugins-2.1.2.tar.gz
 tar xvf nagios-plugins-*.tar.gz
 cd nagios-plugins-*
 #./configure --with-nagios-user=nagios --with-nagios-group=nagios --with-openssl --prefix=${NAGIOS_HOME}
